333fcb677c59da2e87ec463109fe503701043019
anilcosarss <anilcosarsg12@gmail.com>
2023-04-20 18:17:30 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import City from './City';
+
 
 function App() {
+  const key ="46a1fd8e5a459a6ffd9cb7a686d57d29";
+  const [search,setSearch] = useState("");
+  const [city,setCity] =useState();
+  const [isClear,setIsClear] = useState(true)
+  
+
+  useEffect(()=> {
+
+    async function weatherApi(){
+      try{
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${key}&units=metric`)
+        console.log(response) 
+        setCity(response.data); 
+        {response.data.weather[0].main === 'Smoke' || response.data.weather[0].main === 'Clouds' || response.data.weather[0].main === 'Rain' ? setIsClear(true) : setIsClear(false)}
+        
+    
+      } catch(error){
+        console.error(error) 
+      }
+    } 
+    weatherApi(); 
+   
+    },[search]);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App h-screen" style={{
+      backgroundImage: "url(https://img.freepik.com/free-vector/hand-painted-watercolor-pastel-sky-background_23-2148902771.jpg?w=2000)",
+      backgroundSize:"cover",
+      boxShadow:`${isClear && 'inset 10px 8px 140px 102px rgba(2,0,0,0.8)' || 'none' }`  ,
+    }}>
+      <div className='pt-8'>
+        <input  onChange={(e)=> setSearch(e.target.value)}
+         placeholder='Type a city..'
+         type='text'
+        className='px-3 w-[300px] py-3 placeholder-slate-300 text-slate-600 relative  rounded text-sm border border-slate-300 outline-none opacity-50 focus:opacity-100 focus:outline-none w-full' />
+      </div>
+      <City city={city}/>
+       
     </div>
   );
 }
---
+++ src/City.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+function City({ city }) {
+
+    
+    return (
+        <div>
+            {city &&
+                <div>
+                    <h1 className='text-5xl font-bold mt-10 mb-3 italic'>{city.sys.country}</h1>
+                    <h1 className='text-2xl italic bg-stone-400 inline px-3 py-1 rounded-2xl text-white'>{city.name}</h1>
+                    <div className='flex justify-center gap-x-24 my-12 font-normal text-3xl'>
+                        <h1 className='text-4xl font-bold'>{city.main.temp} C°</h1>
+                        <h1 >Humidity: {city.main.humidity}</h1>
+                    </div>
+
+                    <h1 className='text-2xl font-bold mb-2'>{city.weather[0].main}</h1>
+                    <img className='mx-auto' style={{
+                        color: "red",
+                        backgroundColor: "rgba(120, 121, 120, 1)",
+                        borderRadius: "20px"
+                    }} src={`https://openweathermap.org/img/wn/${city.weather[0].icon}@2x.png`} />
+                    <h1 className='italic mt-2 text-lg'>{city.weather[0].description}</h1>
+
+                </div>
+
+            }
+
+
+        </div>
+    )
+}
+
+export default City;
